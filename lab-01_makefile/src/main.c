7806facf2db20545fba88082c37a20acc748e891
Wynell <petrivah@yandex.ru>
2022-11-06 21:43:51 +0300

    Do the code part

---
+++ lab-01_makefile/src/main.c
@@ -0,0 +1,17 @@
+#include "io.h"
+#include "util.h"
+#include "algorithm.h"
+
+int doComputation(int, int);
+
+// int main() {}
+
+// int run() // instead of int main()
+int main()
+{
+  int a = doComputation(5, 7.0);
+  int b = 5;
+  max(a, b);
+  logDebug('!');
+  return 0;
+}
