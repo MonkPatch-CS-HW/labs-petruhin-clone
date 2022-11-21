c25bb9a12c5c26bef7a4895b8a2f26d63aee64b5
Wynell <petrivah@yandex.ru>
2022-11-21 16:22:29 +0300

    Probably working bonus task

--- lab-03_mergesort/src/main.c
+++ lab-03_mergesort/src/main.c
@@ -18,43 +18,55 @@ int comparator_str(const void *a, const void *b)
   return strcmp(*(char *const *)a, *(char *const *)b);
 }
 
-void process_int(int argc, char **argv)
+int process_int(int argc, char **argv)
 {
   int *arr = malloc((argc - 2) * sizeof(int));
   for (int i = 0; i < argc - 2; i++)
     arr[i] = atoi(argv[i + 2]);
-  mergesort(arr, argc - 2, sizeof(int), comparator_int);
+  int res = mergesort(arr, argc - 2, sizeof(int), comparator_int);
+  if (res < 0)
+  {
+    free(arr);
+    return res;
+  }
   for (int i = 0; i < argc - 2; i++)
     printf("%d%c", arr[i], i == argc - 3 ? '\n' : ' ');
   free(arr);
+  return 0;
 }
 
-void process_char(int argc, char **argv)
+int process_char(int argc, char **argv)
 {
   char *arr = malloc((argc - 2) * sizeof(int));
   for (int i = 0; i < argc - 2; i++)
     arr[i] = argv[i + 2][0];
-  mergesort(arr, argc - 2, sizeof(char), comparator_char);
+  int res = mergesort(arr, argc - 2, sizeof(char), comparator_char);
+  if (res < 0)
+  {
+    free(arr);
+    return res;
+  }
   for (int i = 0; i < argc - 2; i++)
     printf("%c%c", arr[i], i == argc - 3 ? '\n' : ' ');
   free(arr);
+  return 0;
 }
 
-void process_str(int argc, char **argv)
+int process_str(int argc, char **argv)
 {
   char **arr = malloc((argc - 2) * sizeof(char *));
   for (int i = 0; i < argc - 2; i++)
+    arr[i] = argv[i + 2];
+  int res = mergesort(arr, argc - 2, sizeof(char *), comparator_str);
+  if (res < 0)
   {
-    arr[i] = malloc((strlen(argv[i + 2]) + 1) * sizeof(char));
-    strcpy(arr[i], argv[i + 2]);
+    free(arr);
+    return res;
   }
-  mergesort(arr, argc - 2, sizeof(char *), comparator_str);
   for (int i = 0; i < argc - 2; i++)
-  {
     printf("%s%c", arr[i], i == argc - 3 ? '\n' : ' ');
-    free(arr[i]);
-  }
   free(arr);
+  return 0;
 }
 
 int main(int argc, char **argv)
@@ -62,15 +74,25 @@ int main(int argc, char **argv)
   if (argc < 2)
   {
     printf("incorrect usage\n");
-    return 0;
+    return 2;
   }
+  int res = 0;
   if (strcmp(argv[1], "int") == 0)
-    process_int(argc, argv);
+    res = process_int(argc, argv);
   else if (strcmp(argv[1], "char") == 0)
-    process_char(argc, argv);
+    res = process_char(argc, argv);
   else if (strcmp(argv[1], "str") == 0)
-    process_str(argc, argv);
+    res = process_str(argc, argv);
   else
+  {
     printf("incorrect type\n");
+    return 3;
+  }
+
+  if (res < 0)
+  {
+    printf("Error: memory allocation failed.");
+    return 1;
+  }
   return 0;
 }
--- lab-03_mergesort/src/mergesort.c
+++ lab-03_mergesort/src/mergesort.c
@@ -3,17 +3,19 @@
 #include <stdio.h>
 #include "mergesort.h"
 
-void merge(
+int merge(
     void *array,
     size_t mid,
     size_t elements, size_t element_size,
     int (*comparator)(const void *, const void *))
 {
   if (elements <= 1)
-    return;
+    return 0;
   void *pivot = (char *)array + element_size * mid;
   void *end = (char *)array + element_size * elements;
   void *out = malloc(elements * element_size);
+  if (out == NULL)
+    return -1;
   void *l = array, *r = pivot, *o = out;
 
   while (l < pivot || r < end)
@@ -21,21 +23,27 @@ void merge(
     int append_l = l < pivot && (r >= end || comparator(l, r) <= 0);
     void **part = append_l ? &l : &r;
     for (size_t i = 0; i < element_size; i++)
-      *(char*)o++ = *(char*)(*part)++;
+      *(char *)o++ = *(char *)(*part)++;
   }
   memcpy(array, out, element_size * elements);
   free(out);
+  return 0;
 }
 
-void mergesort(
+int mergesort(
     void *array,
     size_t elements, size_t element_size,
     int (*comparator)(const void *, const void *))
 {
   if (elements < 2)
-    return;
+    return 0;
   size_t mid = elements / 2;
-  mergesort(array, mid, element_size, comparator);
-  mergesort((char *)array + mid * element_size, elements - mid, element_size, comparator);
-  merge(array, mid, elements, element_size, comparator);
+  int res;
+  if ((res = mergesort(array, mid, element_size, comparator)) < 0)
+    return res;
+  if ((res = mergesort((char *)array + mid * element_size, elements - mid, element_size, comparator)) < 0)
+    return res;
+  if ((res = merge(array, mid, elements, element_size, comparator)) < 0)
+    return res;
+  return res;
 }
