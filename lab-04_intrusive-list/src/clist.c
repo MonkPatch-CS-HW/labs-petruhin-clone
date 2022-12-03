80f887f51e3b5917d22e4f5f80cb061083f959c2
Wynell <petrivah@yandex.ru>
2022-12-03 18:55:24 +0300

    Make intrusive list work in both directions and add function that adds an element to its back{

--- lab-04_intrusive-list/src/clist.c
+++ lab-04_intrusive-list/src/clist.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "clist.h"
 
 void init_list(intrusive_list_t *list)
@@ -13,14 +14,44 @@ void uninit_list(intrusive_list_t *list)
   free(list->head);
 }
 
-void add_node(intrusive_list_t *list, intrusive_node_t *node)
+int try_insert_first_node(intrusive_list_t *list, intrusive_node_t *node)
 {
   if (!list->head)
     init_list(list);
+
+  if (list->head->next || list->head->prev)
+    return -1;
+
+  list->head->next = node;
+  list->head->prev = node;
+
+  node->next = NULL;
+  node->prev = NULL;
+
+  return 0;
+}
+
+void add_node_back(intrusive_list_t *list, intrusive_node_t *node)
+{
+  if (try_insert_first_node(list, node) == 0)
+    return;
+
+  node->prev = list->head->prev;
+  node->next = NULL;
+
+  node->prev->next = node;
+  list->head->prev = node;
+}
+
+void add_node(intrusive_list_t *list, intrusive_node_t *node)
+{
+  if (try_insert_first_node(list, node) == 0)
+    return;
+
   node->next = list->head->next;
-  node->prev = list->head;
-  if (list->head->next)
-    list->head->next->prev = node;
+  node->prev = NULL;
+
+  node->next->prev = node;
   list->head->next = node;
 }
 
@@ -31,6 +62,10 @@ void remove_node(intrusive_list_t *list, intrusive_node_t *node)
     ;
   if (!current)
     return;
+  if (list->head->next == node)
+    list->head->next = node->next;
+  if (list->head->prev == node)
+    list->head->prev = node->prev;
   if (node->next)
     node->next->prev = node->prev;
   if (node->prev)
