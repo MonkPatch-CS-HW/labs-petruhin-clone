1e45c5e4b62663b785c964aa979dedb952d10546
Wynell <petrivah@yandex.ru>
2022-12-09 13:29:01 +0300

    Make main() return non-zero return code in case of any error

--- hw-01_bmp/src/main.c
+++ hw-01_bmp/src/main.c
@@ -4,41 +4,47 @@
 #include <stddef.h>
 #include "bmp.h"
 
-void process_crop_rotate(bmp_picture_t *bmp, char *infile, char *outfile, int x, int y, int w, int h)
+/**
+ * Return codes:
+ * 0 on success,
+ * -1 on any error
+*/
+int process_crop_rotate(bmp_picture_t *bmp, char *infile, char *outfile, int x, int y, int w, int h)
 {
-  int res;
+  int res = 0;
 
   if ((res = load_bmp(infile, bmp)) != 0)
   {
     printf("loading bmp failed... error code: %d\n", res);
-    return;
+    return -1;
   }
 
   if ((res = crop(bmp, x, y, w, h)) != 0)
   {
     printf("cropping bmp failed, error code: %d\n", res);
-    return;
+    return -1;
   }
 
   if ((res = rotate(bmp)) != 0)
   {
     printf("rotating bmp failed, error code: %d\n", res);
-    return;
+    return -1;
   }
-
-  printf("byte_size=%d\n", bmp->header.bf_size);
 
   if ((res = save_bmp(outfile, bmp)) != 0)
   {
     printf("saving bmp failed, error code: %d\n", res);
-    return;
+    return -1;
   }
+
+  return 0;
 }
 
 /**
  * Return codes:
  * 0 on success,
  * -1 on incorrect usage
+ * -2 on command error
 */
 int process_cmd(int argc, char **argv, bmp_picture_t *bmp)
 {
@@ -55,7 +61,8 @@ int process_cmd(int argc, char **argv, bmp_picture_t *bmp)
     int y = atoi(argv[5]);
     int w = atoi(argv[6]);
     int h = atoi(argv[7]);
-    process_crop_rotate(bmp, infile, outfile, x, y, w, h);
+    if (process_crop_rotate(bmp, infile, outfile, x, y, w, h) != 0)
+      return -2;
   }
 
   return 0;
@@ -63,10 +70,15 @@ int process_cmd(int argc, char **argv, bmp_picture_t *bmp)
 
 int main(int argc, char **argv)
 {
+  int res, ret = 0;
   bmp_picture_t *bmp = malloc(sizeof(bmp_picture_t));
-  if (process_cmd(argc, argv, bmp) != 0)
-    printf("incorrect usage\n");
+  if ((res = process_cmd(argc, argv, bmp)) != 0)
+  {
+    if (res == -1)
+      printf("incorrect usage\n");
+    ret = 1;
+  }
   unload_bmp(bmp);
   free(bmp);
-  return 0;
+  return ret;
 }
