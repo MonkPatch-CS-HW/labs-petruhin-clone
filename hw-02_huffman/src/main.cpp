fc9a3aee939ded77ef9d851949aee02264d569c6
Ivan Petrukhin <monkpatch@gmail.com>
2023-04-30 16:51:27 +0300

    Improve error handling

--- hw-02_huffman/src/main.cpp
+++ hw-02_huffman/src/main.cpp
@@ -12,30 +12,53 @@ int main(int argc, char **argv) {
   CliArguments args;
 
   HuffmanCompressor::CompressorStats stats;
-  args.parse(argc, argv);
+  try {
+    args.parse(argc, argv);
+  } catch (std::exception &ex) {
+    std::cerr << "error parsing arguments: " << ex.what() << std::endl;
+    return 1;
+  }
 
   if (args.inputFile.size() == 0) {
-    std::cerr << "Specify input file with -f or --file" << std::endl;
+    std::cerr << "specify input file with -f or --file" << std::endl;
     return 1;
   }
 
   if (args.outputFile.size() == 0) {
-    std::cerr << "Specify output file with -o or --output" << std::endl;
+    std::cerr << "specify output file with -o or --output" << std::endl;
     return 1;
   }
 
   std::ifstream fin(args.inputFile);
   std::ofstream fout(args.outputFile);
 
+  if (!fin.is_open()) {
+    std::cerr << "error opening input file" << std::endl;
+    return 1;
+  }
+
+  if (!fout.is_open()) {
+    std::cerr << "error opening output file" << std::endl;
+    return 1;
+  }
+
   switch (args.mode) {
   case CliArguments::MODE_COMPRESS:
-    stats = hc.compress(fin, fout);
+    try {
+      stats = hc.compress(fin, fout);
+    } catch (std::exception &ex) {
+      std::cerr << "error compressing file: " << ex.what() << std::endl;
+    }
     break;
   case CliArguments::MODE_DECOMPRESS:
-    stats = hc.decompress(fin, fout);
+    try {
+      stats = hc.decompress(fin, fout);
+    } catch (std::exception &ex) {
+      std::cerr << "error decompressing file: " << ex.what() << std::endl;
+    }
     break;
   default:
-    std::cerr << "Specify mode with -c or -u" << std::endl;
+    std::cerr << "specify mode with -c or -u" << std::endl;
     return 1;
   }
 
